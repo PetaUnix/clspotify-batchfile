b9d3724d5acff68b1d7e72b396c7fbd6eb9667b7
PetaUnix <cranny292@gmail.com>
2022-01-24 15:31:10 +0800

    added batch file

---
+++ Clspotify.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+:start
+python zspotify -s
+pause
+cls
+goto start
---
+++ Requirements.bat
@@ -0,0 +1,5 @@
+@echo off
+pip install -r requirements.txt
+echo Do you want to delete this Requirements.bat file?
+del /p Requirements.bat
+pause
